9c4d0a4ec1bce910d71ecfc35d6742714651fae3
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2020-12-16 11:01:12 +0700

    Bach updated greedy.cpp 11:01 a.m

--- greedy.cpp
+++ greedy.cpp
@@ -1,8 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define i2 pair<int,int>
+
 vector<int> adj[100];
 int n;
+
+void greedy_coloring(vector<int> &result) {
+    result.clear();
+    for (int i = 0; i < n; ++i) result.push_back(0);
+
+    int color = 1;
+
+    while (true) {
+        priority_queue<i2, vector<i2> > Q;
+        for (int i = 0; i < n; ++i) { ///For step 4: eliminate colored vertices and resort the whole set
+            if (result[i] == 0) Q.push({adj[i].size(), i});
+        }
+
+        if (Q.empty()) break; ///No more uncolored vertices
+
+        set<int> eliminated; ///For step 2: dont touch vertices that are adjacent to vertices colored i
+        while(!Q.empty()) {
+            i2 u = Q.top();
+            Q.pop();
+            if (eliminated.find(u.second)==eliminated.end()) {
+                result[u.second] = color;
+                for (int j : adj[u.second]) eliminated.insert(j); ///eliminate all adjacent vertices
+            }
+        }
+        color++;
+    }
+}
+
 
 int main() {
 
@@ -24,6 +54,11 @@ int main() {
         }
     }
 
+    vector<int> result;
+    greedy_coloring(result);
+
+    for (int i : result) cout << i << " ";
+    cout << endl;
 
     return 0;
 }
